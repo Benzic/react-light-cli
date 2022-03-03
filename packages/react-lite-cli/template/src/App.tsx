fbefbb92d4e4b1e3a8d9353900cdfb283f81514e
benzic <690018019@qq.com>
2022-03-03 09:58:27 +0800

    init leran

--- packages/react-lite-cli/template/src/App.tsx
+++ packages/react-lite-cli/template/src/App.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-01 14:44:16
+ * @LastEditTime: 2022-03-03 09:58:09
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \booster\packages\react-lite-cli\template\src\App.tsx
+ */
 import React from 'react';
 import './App.scss';
 
@@ -6,7 +14,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1 className="hello">
-                    Hello Booster !
+                    Hello Light Cli !
                 </h1>
                 <h3 className='page'>current page is <span><h2><%= pageName %></h2></span></h3>
             </header>
