827e0eb894ab3ab9bcb27c7b5d5b0668c54c607d
benzic <690018019@qq.com>
2022-03-14 15:58:34 +0800

    fixed:fix mobx import bug

--- packages/react-booster-cli/template/src/mobx/components/counter.tsx
+++ packages/react-booster-cli/template/src/mobx/components/counter.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-09 16:09:55
- * @LastEditTime: 2022-03-11 14:27:07
+ * @LastEditTime: 2022-03-14 14:45:15
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \booster\my-project\src\counter.tsx
@@ -9,7 +9,7 @@
 import React from "react"
 import { Button } from "antd"
 import { observer } from 'mobx-react-lite'
-import { useStore } from '../mobx/index';
+import { useStore } from '../store/index';
 const Counter: React.FC = () => {
     const { countStore } = useStore();
     const { addCount, decCount, count } = countStore
